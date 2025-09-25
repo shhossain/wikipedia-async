d6727b023b5a236b7595323f9122ff44272762e1
shhossain <hossain0338@gmail.com>
2025-09-25 22:23:47 +0600

    fix: Update titles and language parameter in batch retrieval for consistency

--- test.py
+++ test.py
@@ -15,17 +15,12 @@ async def main():
     )
     # s = time.time()
     # await client.set_language("bn")
-    # titles = [
-    #     "Python (programming language)",
-    #     "Java (programming language)",
-    #     "C (programming language)",
-    # ]
-
     titles = [
-        "নটর_ডেম_কলেজ_ঢাকা",
-        "https://bn.wikipedia.org/wiki/%E0%A6%AC%E0%A6%BE%E0%A6%82%E0%A6%B2%E0%A6%BE%E0%A6%A6%E0%A7%87%E0%A6%B6",
-        "https://bn.wikipedia.org/wiki/%E0%A6%AD%E0%A6%BE%E0%A6%B0%E0%A6%A4_%E0%A6%AC%E0%A6%BF%E0%A6%AD%E0%A6%BE%E0%A6%9C%E0%A6%A8",
+        "Python (programming language)",
+        "Java (programming language)",
+        "C (programming language)",
     ]
+
     # s = time.time()
     # res = await client.get_page_html_batch(titles)
     # for title, html in zip(titles, [r.html for r in res.successful]):
@@ -44,7 +39,7 @@ async def main():
     # print("Time taken:", time.time() - s)
 
     s = time.time()
-    res = await client.get_pages_batch(titles, lang="bn")
+    res = await client.get_pages_batch(titles, lang="en")
     print("Time taken:", time.time() - s)
     for page in res.successful:
         print("Title:", page.title)
@@ -57,14 +52,14 @@ async def main():
     for fail in res.failed:
         print(f"Failed to get {fail['title']}: {fail['error']}")
 
-    # for page in page.successful:
-    #     print("Title:", page.title)
-    #     print("Summary:", page.summary)
-    #     print(page.helper.tree_view(100))
-    #     print("=" * 80)
-    # for sec in page.sections:
-    #     print(sec.title)
-    #     print(sec.to_string(markdown=True)[:200])
+    for page in res.successful:
+        print("Title:", page.title)
+        print("Summary:", page.summary)
+        print(page.helper.tree_view(100))
+        print("=" * 80)
+        for sec in page.sections:
+            print(sec.title)
+            print(sec.to_string(markdown=True)[:200])
 
     # print(list(page.tables.items())[3])
 
